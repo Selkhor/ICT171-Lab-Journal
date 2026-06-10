8631be6107e967ce9104d38e910067003b181bbe
Selkhor <163364862+Selkhor@users.noreply.github.com>
2026-06-10 18:16:45 +0800

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-echo "Starting backup process..."
-
 DATE=$(date +%Y%m%d_%H%M%S)
-
-echo "Backup completed on $DATE"
+mkdir -p /home/ubuntu/backup
+cp -R /home/ubuntu/Documents/* /home/ubuntu/backup/
+zip -r "/home/ubuntu/backup_$DATE.zip" /home/ubuntu/backup/*
+echo "Backup completed at $(date)"
